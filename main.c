318f35385fe63cbc290daf1cfd1eb3795a2e0383
“madalenarb” <mariamadalenarob@tecnico.ulisboa.pt>
2021-03-28 21:34:59 +0100

    i

--- main.c
+++ main.c
@@ -7,20 +7,22 @@
 #define DEFAULT_MODO_POSICIONAMENTO 1
 #define DEFAULT_MODO_DISPARO 1
 
+//função que será executada quando o utilizador escreve -h no terminal
 void instrucoes()
 {
     printf("B A T A L H A   N A V A L\n\n");
     printf("Modo de utilização: ./wargame[opções]\n");
-    printf("Opções:\n");
+    printf("opções:\n");
     printf("-h\t\t    ajuda para o utilizador\n");
     printf("-t\t\t    dimensão do tabuleiro (linha x coluna)\n");
     printf("-j\t\t    modo de jogo\n");
     printf("-p\t\t    modo de posicionamento de peças pelo computador (1 a 2)\n");
     printf("-d\t\t    modo de disparo de peças pelo computador (1 a 3)\n");
     printf("-[1-8]\t\t    indica o número de peças de cada tipo (mínimo 1)\n\n");
+
 }
 
 int main()
 {
-instrucoes();
+    instrucoes();
 }
