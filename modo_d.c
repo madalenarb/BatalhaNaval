680a1dcf6e4b49bc58f089082953650c1ba7faaa
JoseMontez <joseantoniomontez758@gmail.com>
2021-04-21 23:37:40 +0100

    correçao

--- modo_d.c
+++ modo_d.c
@@ -1,5 +1,5 @@
 #include "modo_d.h"
-
+//esta funçao verifica no modo de jogo 1 se todas as peças do tabuleiro ja foram encontradas
 int verificar_tab(int tabuleiro[25][35], int linhas, int colunas){
     int i,j;
     for (i = 0; i < linhas; i++){
@@ -12,7 +12,7 @@ int verificar_tab(int tabuleiro[25][35], int linhas, int colunas){
     }
     return 0;  
 }
-
+//funçao de disparo1, o computador dispara aleatoriamente até atinjir em todas as peças
 void disparo_1(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, int disparosMax, int linhas, int colunas){
     int randCol = 0;
     int randLin = 0;
@@ -49,7 +49,8 @@ void disparo_1(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, i
         }         
     }       
 }
-
+//esta funçao serve para o utilizador dar o input de resposta aos disparos do computador
+//o input tem que ser de um carater apenas: 1-8 caso o computador acerte numa peça e '-' caso atinja água
 int resposta(char charCol, char charLin)
 {
     int id_peca = 0;
@@ -80,7 +81,8 @@ int verificar_matriz(int tabuleiro[25][35]){
     }
  return 0;
 }
-
+//esta matriz verifica se as peças numa matriz 3x3 ja foram todas encontradas pelo computador
+//por exemplo: caso o computador dispare numa peça 3, quando disparar nas restantes duas peças 3, avança para a proxima matriz3x3
 int verificar_mat(int tabuleiro3[25][35], int id_peca, int a, int b){
    int i, j;
    int numpecas = 0;
@@ -97,6 +99,8 @@ int verificar_mat(int tabuleiro3[25][35], int id_peca, int a, int b){
     return 1;
 }
 
+/*funçao de disparo2: o computador dispara de acordo com uma sequencia, ate atinjir todas as peças numa matriz3x3
+, e depois avança para a matriz seguinte*/
 
 void disparo_2(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, int linhas,int colunas){
     int flag;
@@ -111,8 +115,8 @@ void disparo_2(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, i
     int flag2 = 1;
     apagar_tabuleiro(tabuleiro3, linhas, colunas);
     while(flag == 1 && disparosMin > 0){
-        for (a = 0; a < linhas;  a += 3){
-            for ( b = 0; b < colunas; b += 3){
+        for (b = 0; b < colunas;  b += 3){
+            for ( a = 0; a < linhas; a += 3){
                 flag2 = 1;
                 i = 0;
                 while(flag2 == 1 && i < 9){
@@ -145,6 +149,8 @@ void disparo_2(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, i
         }      
     }
 }
+/*funçao de disparo3, semelhante ao disparo 2 mas inclui tmb a funcao de restricaodisparo3: o computador n dispara
+numa posiçao adjacente a uma posiçao ja preenchida  */
 void disparo_3(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, int linhas,int colunas){
     int flag;
     int i = 0;
@@ -155,12 +161,14 @@ void disparo_3(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, i
     int seqlin[] = { 1,0,2,1,1,0,2,0,2};
     int seqcol[] = { 1,1,1,0,2,0,2,2,0};
     flag = 1;
+    /*a flag2 devolve 0 caso as peças numa matriz3x3 ja tinham sido todas preenchidas (para verificar usamos a funçao 
+    verificar_mat)*/
     int flag2 = 1;
     apagar_tabuleiro(tabuleiro3, linhas, colunas);
     apagar_tabuleiro(tabuleiro, linhas, colunas);
     while(flag == 1 && disparosMin > 0){
-        for (a = 0; a < linhas;  a += 3){
-            for ( b = 0; b < colunas; b += 3){
+        for (b = 0; b < colunas;  b += 3){
+            for ( a = 0; a < linhas; a += 3){
                 flag2 = 1;
                 i = 0;
                 
@@ -196,6 +204,8 @@ void disparo_3(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, i
         }      
     }
 }
+/*esta funçao serve para aplicar a restriçao no disparo 3 (o tabuleiro1 fica a 9,
+ indicando ao computador que nao pode disparar nestas posiçoes)*/
 void restricaodisparo3(int tabuleiro3[25][35], int tabuleiro[25][35], int a, int b){
     int i,k,d;
     for(i=0; i<3; i++){
