069a2e57925d8f60fba931c1a7920f9f3b4cceb3
madalenarb <mariamadalenarob@tecnico.ulisboa.pt>
2021-04-21 15:55:06 +0100

    1

--- modo_d.h
+++ modo_d.h
@@ -3,7 +3,8 @@
 #include "tabuleiro.h"
 
 int verificar_tab(int tabuleiro[25][35], int colunas, int linhas);
-void disparo_1(int tabuleiro[25][35],int tabuleiro3[25][35], int disparosMin,int colunas, int linhas);
+int resposta(char charCol, char charLin);
+void disparo_1(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, int disparosMax, int linhas, int colunas);
 int verificar_matriz(int tabuleiro[25][35]);
-void disparo_2(int tabuleiro[25][35],  int tabuleiro3[25][35]);
+void disparo_2(int tabuleiro[25][35], int tabuleiro3[25][35], int linhas, int colunas, int submat);
 void disparo_3(int tabuleiro[25][35],int tabuleiro3[25][35]);
