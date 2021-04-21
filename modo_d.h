68a7545f73290015f2dcf2b421d85215e55a9848
madalenarb <78974570+madalenarb@users.noreply.github.com>
2021-04-21 17:04:22 +0100

    Add files via upload

--- modo_d.h
+++ modo_d.h
@@ -6,11 +6,6 @@ int verificar_tab(int tabuleiro[25][35], int colunas, int linhas);
 int resposta(char charCol, char charLin);
 void disparo_1(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, int disparosMax, int linhas, int colunas);
 int verificar_matriz(int tabuleiro[25][35]);
-<<<<<<< HEAD
-void disparo_2(int tabuleiro[25][35],  int tabuleiro3[25][35],int disparosMin,int linhas,int colunas);
+int verificar_mat(int tabuleiro3[25][35], int id_peca, int a, int b);
+void disparo_2(int tabuleiro[25][35], int tabuleiro3[25][35], int disparosMin, int linhas,int colunas);
 void disparo_3(int tabuleiro[25][35],int tabuleiro3[25][35]);
-int verficar_mat(int tabuleiro3[25][35], int id_pecanum, int a,int b);
-=======
-void disparo_2(int tabuleiro[25][35], int tabuleiro3[25][35], int linhas, int colunas, int submat);
-void disparo_3(int tabuleiro[25][35],int tabuleiro3[25][35]);
->>>>>>> 069a2e57925d8f60fba931c1a7920f9f3b4cceb3
