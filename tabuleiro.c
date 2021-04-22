6cdaa76172eff5f759dc4a726d112c9476bb36f8
madalenarb <mariamadalenarob@tecnico.ulisboa.pt>
2021-04-22 16:42:28 +0100

    1

--- tabuleiro.c
+++ tabuleiro.c
@@ -44,7 +44,6 @@ void apagar_tabuleiro( int tabuleiro[25][35], int linhas, int colunas )
             tabuleiro[i][j] = 0;
             }
     }
-    printf("\n");
 }
 
 void apagar_submat(int tabuleiro[25][35], int poslinha, int poscoluna)
